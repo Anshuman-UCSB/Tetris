d2bbeabc3bd70a9e284667db5c022d086c69f376
Anshuman Dash <anshuman@ucsb.edu>
2023-12-26 01:12:42 -0800

    All pieces!

--- Backend/game.py
+++ Backend/game.py
@@ -1,15 +1,39 @@
+from random import shuffle
 class Game:
 	def __init__(self):
 		self.grid = [[[0,"bg"] for _ in range(10)] for _ in range(20)]
 		for i,p in enumerate("IOTJLSZ"):
-			self.grid[0][i] = (1,p)
+			self.grid[0][i] = (0,p)
 		self.activePiece = []
 		self.activeType = None
 		self.alive = True
+		self.nextPieces = self.pieceBag()
 		# id, color
+	def pieceBag(self):
+		choices = list("IOTJLSZ")
+		shuffle(choices)
+		return choices
 	def createPiece(self):
-		self.activePiece = [(5,0),(6,0),(5,1),(6,1)]
-		self.activeType = 'O'
+		self.activeType = self.nextPieces.pop(0)
+		if len(self.nextPieces) < 7:
+			self.nextPieces+=self.pieceBag()
+		match(self.activeType):
+			case "I":
+				self.activePiece = [(5,0),(6,0),(7,0),(8,0)]
+			case "O":
+				self.activePiece = [(5,0),(6,0),(5,1),(6,1)]
+			case "T":
+				self.activePiece = [(5,0),(6,0),(4,0),(5,1)]
+			case "J":
+				self.activePiece = [(5,0),(6,0),(4,0),(6,1)]
+			case "L":
+				self.activePiece = [(5,0),(6,0),(4,0),(4,1)]
+			case "Z":
+				self.activePiece = [(5,0),(6,0),(5,1),(4,1)]
+			case "S":
+				self.activePiece = [(5,0),(4,0),(5,1),(6,1)]
+			case _:
+				self.activePiece = [(4,0),(5,0),(6,0),(5,1),(5,2)]
 		for x,y in self.activePiece:
 			self.grid[y][x] = [2,self.activeType]
 	def render(self):
