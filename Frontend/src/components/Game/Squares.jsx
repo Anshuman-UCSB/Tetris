1b9a669e1d0f7682a133996d0979d3ae811d1e14
Anshuman Dash <anshuman@ucsb.edu>
2024-01-09 02:12:47 -0800

    WIP, but pulling from api

--- Frontend/src/components/Game/Squares.jsx
+++ Frontend/src/components/Game/Squares.jsx
@@ -11,7 +11,7 @@ export function Square({ color_ind }){
     "g": "#a2a6a6"
   };
   return (
-    <div style={{backgroundColor: colors[color_ind]}} className='grow pt-[10%] w-full '/>
+    <div style={{backgroundColor: colors[color_ind]}} className='border-[1px] grow pt-[10%] w-full '/>
   )
 }
 export function EmptySquare({ color_ind }){
